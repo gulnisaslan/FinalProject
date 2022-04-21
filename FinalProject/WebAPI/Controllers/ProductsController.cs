77d0fac0ad08dc7a8fa545dafc76df87bc203bf3
gulnisaslan <gulnisaslan@gmail.com>
2022-04-21 17:00:40 +0300

    Final project for kodlama.io. this repo is current

---
+++ FinalProject/WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,80 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //loosely coupled
+        //naming Convetion
+        //Ioc Container -->inversion of control
+       
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            //Thread.Sleep(1000);
+         //   dependecy chain
+
+
+            var result =  _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+        [HttpPost("add") ]
+        public IActionResult Add(Product product)
+
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+
+        }
+        [HttpPost("getbyId")]
+        public IActionResult GetById(int id )
+        {
+            var result = _productService.GetAllById(id);
+            if (result.Success)
+            {
+                return Ok(result.Success);
+            }
+            return BadRequest(result);
+
+        }
+        [HttpPost("getbyCategoryId")]
+        public IActionResult GetByCategory(int categoryId)
+        {
+            var result = _productService.GetAllById(categoryId);
+            if (result.Success)
+            {
+                return Ok(result.Success);
+            }
+            return BadRequest(result);
+
+        }
+    }
+}
